648a301f1e53ede5cd6b50dc610002b1c76da19c
Anastasiia <anastasiia.petrovych@ucu.edu.ua>
2024-05-24 23:28:47 +0300

    Update constants.py

--- src/category_a/constants.py
+++ src/category_a/constants.py
@@ -1,8 +1,9 @@
-"""
-Constants for the projects
-"""
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Kafka configurations
-KAFKA_BROKER = "kafka:9092"
-CONSUMER_TOPIC = ""
-PRODUCER_TOPIC = ""
+KAFKA_BROKER = os.getenv('KAFKA_BROKER')
+CONSUMER_TOPIC = os.getenv('CONSUMER_TOPIC')
+PRODUCER_TOPIC = os.getenv('PRODUCER_TOPIC')
