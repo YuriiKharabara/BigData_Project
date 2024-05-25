0625e528bcc85405d14e17b316be36523523b19a
Anastasiia <anastasiyapetrovych25@gmail.com>
2024-05-25 15:49:16 +0300

    Add the main file

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import uvicorn as uvicorn
+from fastapi import FastAPI
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+
+from src.category_a.router import statistics_router
+from src.category_b.router import pages_router
+
+app = FastAPI(
+    docs_url='/docs',
+    middleware=[
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=['*'],
+        )
+    ]
+)
+
+app.include_router(statistics_router, tags=['stats'])
+app.include_router(pages_router, tags=['pages'])
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8083)
