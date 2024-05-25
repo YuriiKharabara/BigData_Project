0797798e7fbf8c604f000549b00e6b809307adf3
Anastasiia <anastasiyapetrovych25@gmail.com>
2024-05-25 15:44:34 +0300

    Add category-b

---
+++ src/category_b/router.py
@@ -0,0 +1,70 @@
+"""
+"""
+
+from fastapi import APIRouter, Query
+
+from src.category_b import service
+from src.handle_generic_exceptions import handle_and_raise_generic_exception
+
+pages_router = APIRouter(prefix='/pages')
+
+@handle_and_raise_generic_exception
+@pages_router.get("/domains")
+def get_domains():
+    """
+    Return the list of existing domains for which pages were created.
+
+    :return: (list) - The existing domains for created pages.
+    """
+    domains = service.get_existing_domains()
+    return domains
+
+
+@handle_and_raise_generic_exception
+@pages_router.get("/pages")
+def get_pages_by_user_id(user_id: str = Query(..., description="User ID.")):
+    """
+    Return all the pages which were created by the user with a specified user ID.
+
+    :return: (list) - The pages with specified user ID.
+    """
+    pages = service.get_pages_by_user_id(user_id)
+    return pages
+
+
+@handle_and_raise_generic_exception
+@pages_router.get("/articles")
+def get_articles_by_domain(domain: str = Query(..., description="Specified domain")):
+    """
+    Return the number of articles created for a specified domain.
+
+    :return: (int) - The number of articles.
+    """
+    articles = service.get_articles_by_domain(domain)
+    return articles
+
+
+@handle_and_raise_generic_exception
+@pages_router.get("/page")
+def get_page_by_id(page_id: str = Query(..., description="Page ID")):
+    """
+    Return the page with the specified page ID.
+
+    :return: (list) - The given page.
+    """
+    page = service.get_page_by_id(page_id)
+    return page
+
+
+@handle_and_raise_generic_exception
+@pages_router.get("/users")
+def get_page_by_id(start_time: str = Query(..., description="The start of period"),
+                   end_time: str = Query(..., description="The end of period")):
+    """
+    Return the id, name, and the number of created pages of all the users who created at least one page in a specified
+    time range.
+
+    :return: (list) - The received users.
+    """
+    users = service.get_users(start_time, end_time)
+    return users
---
+++ src/category_b/service.py
@@ -0,0 +1,18 @@
+def get_existing_domains():
+    pass
+
+
+def get_pages_by_user_id(user_id):
+    pass
+
+
+def get_articles_by_domain(domain):
+    pass
+
+
+def get_page_by_id(page_id):
+    pass
+
+
+def get_users(start_time, end_time):
+    pass
