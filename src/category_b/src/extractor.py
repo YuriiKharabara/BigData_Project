327f046a8381beb298e334d7132c0fadadc17522
Anastasiia <anastasiyapetrovych25@gmail.com>
2024-05-26 03:02:06 +0300

    Add proper RequestException handler to automatically relaunch extractor

--- src/category_b/src/extractor.py
+++ src/category_b/src/extractor.py
@@ -1,4 +1,6 @@
 import json
+import time
+
 import requests
 from kafka import KafkaProducer
 import logging
@@ -9,25 +11,30 @@ producer = KafkaProducer(bootstrap_servers=[constants.KAFKA_BROKER],
 
 
 def send_messages():
-    try:
-        for line in requests.get(constants.URL, stream=True).iter_lines():
-            if line:
-                line = line.decode('utf-8').strip()
-                if line.startswith('data:'):
-                    json_str = line.split('data:', 1)[1].strip()
-                    try:
-                        event = json.loads(json_str)
-                        logging.info(event)
-                        producer.send(constants.KAFKA_TOPIC, event)
-                        producer.flush()
-                    except json.JSONDecodeError as e:
-                        logging.info(f"Error decoding JSON: {e} - Line: {json_str}")
-                    except Exception as e:
-                        logging.info(f"An error occurred: {e}")
-                else:
-                    logging.info(f"Skipping non-data line: {line}")
-    except Exception as e:
-        send_messages()
+    while True:
+        try:
+            for line in requests.get(constants.URL, stream=True).iter_lines():
+                if line:
+                    line = line.decode('utf-8').strip()
+                    if line.startswith('data:'):
+                        json_str = line.split('data:', 1)[1].strip()
+                        try:
+                            event = json.loads(json_str)
+                            logging.info(f"Sending event: {event}")
+                            producer.send(constants.KAFKA_TOPIC, event)
+                            producer.flush()
+                        except json.JSONDecodeError as e:
+                            logging.error(f"Error decoding JSON: {e} - Line: {json_str}")
+                        except Exception as e:
+                            logging.error(f"An error occurred: {e}")
+                    else:
+                        logging.info(f"Skipping non-data line: {line}")
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error with HTTP request: {e}")
+            time.sleep(10)
+        except Exception as e:
+            logging.error(f"An unknown error occurred: {e}")
+            time.sleep(10)
 
 
 if __name__ == "__main__":
